62f7bb89baba0919ce1dcf8ce946de7c665b1681
Pedro Muniz <pedropmun.spam@gmail.com>
2023-04-08 23:44:40 -0300

    changing rendered data

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const Test = () => {
   const [data, useData] = React.useState(false)
   console.log('running 2', String(data), data.toString())
-  return <div onClick={() => {useData(true)}}>{data}</div>
+  return <div onClick={() => {useData(true)}}>{`${data}`}</div>
 }
