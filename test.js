2ae01041d2fe872afca6322c064ce4288f76b5aa
Pedro Muniz <pedropmun.spam@gmail.com>
2023-04-08 23:32:35 -0300

    test data value

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const Test = () => {
   const [data, useData] = React.useState(false)
-  console.log('running')
+  console.log('running', String(data))
   return <div onClick={() => {useData(true)}}>{data}</div>
 }
