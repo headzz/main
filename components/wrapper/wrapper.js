652146f3b7ab11443d7f766f2d7f558f11aa5844
Pedro Muniz <pedropmun.spam@gmail.com>
2023-04-09 22:38:34 -0300

    adding animation opacity

--- components/wrapper/wrapper.js
+++ components/wrapper/wrapper.js
@@ -2,5 +2,6 @@ const { styled } = window;
 
 const Wrapper = styled.div({
   backgroundImage: 'linear-gradient(#8c32cf, #3c0065)',
-  height: '100vh'
+  height: '100vh',
+  animation: 'opac 2.8s'
 });
