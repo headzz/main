9add3ab3b1becbb67e63820a5cdab923d8698fe0
Pedro Muniz <pedropmun.spam@gmail.com>
2023-04-09 21:04:00 -0300

    fixing styled for text

--- components/text/text.js
+++ components/text/text.js
@@ -1,6 +1,8 @@
 const Text = styled('p')`
-  fontFamily:  "Jost","Helvetica Neue",Helvetica,Arial,sans-serif,
-  fontWeight: ({weight}) => weight ? weight : 400,
-  fontSize: ({size}) => size ? size : '1.6rem',
-  fontStyle: ({fontStyle}) => fontStyle ? fontStyle : 'normal'
+  font-family:  "Jost","Helvetica Neue",Helvetica,Arial,sans-serif,
+  font-weight: ({weight}) => weight ? weight : 400,
+  font-size: ({size}) => size ? size : '1.6rem',
+  font-style: ({fontStyle}) => fontStyle ? fontStyle : 'normal'
 `;
+
+
