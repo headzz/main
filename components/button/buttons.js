18defb712459069a60b61383f6ac906fbc1248ed
Pedro Muniz <pedropmun.spam@gmail.com>
2023-04-10 01:14:51 -0300

    fixing button hover color

--- components/button/buttons.js
+++ components/button/buttons.js
@@ -2,5 +2,5 @@ const ButtonInvisible = styled('button')({
   border: 0,
   background: 0,
   color: ({color}) => color ? color : '#fff',
-  [':hover']: {color: '#e9e800'}
+  ['&:hover']: {color: '#e9e800'}
 })
--- components/text/text.js
+++ components/text/text.js
@@ -4,5 +4,4 @@ const Text = styled('p')({
   fontWeight: ({weight}) => weight ? weight : 400,
   fontSize: ({size}) => size ? size : '1.6rem',
   fontStyle: ({fontStyle}) => fontStyle ? fontStyle : 'normal',
-  ['@media (min-width: 600px)']: {color: '#155ca3'}
 })
