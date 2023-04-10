dee6d26e0800556784df65a2786a4ed3191c56ac
Pedro Muniz <pedropmun.spam@gmail.com>
2023-04-10 02:10:47 -0300

    finishing styling buttons

--- components/button/buttons.js
+++ components/button/buttons.js
@@ -5,17 +5,17 @@ const ButtonInvisible = styled.button({
   ["&:focus > p"]: { color: '#e9e800', borderBottom: "2px solid #e9e800" },
 })
 
-const SecondaryButton = styled.button({
-  border: '1px solid #fff',
-  backgroundColor: '#8737c7',
-  ["&:hover > p"]: {backgroundColor: '#e9e800', color: '#3a0065', border: 0},
-  ["&:focus > p"]: { backgroundColor: '#fff', color: '#3a0065', border: '2px solid $$4883ec3' },
-})
-
 const PrimaryButton = styled.button({
   border: '1px solid #fff',
   backgroundColor: '#e9e800',
   ["& > p"]: { color: '#3a0065'},
   ["&:hover > p"]: {backgroundColor: '#3a0065', color: '#fff', border: 0},
-  ["&:focus > p"]: { backgroundColor: '#fff', color: '#3a0065' },
+  ["&:focus > p"]: { backgroundColor: '#fff', color: '#3a0065', border: '2px solid #883ec3' },
+})
+
+const SecondaryButton = styled.button({
+  border: '1px solid #fff',
+  backgroundColor: '#8737c7',
+  ["&:hover > p"]: {backgroundColor: '#e9e800', color: '#3a0065', border: 0},
+  ["&:focus > p"]: { backgroundColor: '#fff', color: '#3a0065', border: '2px solid #883ec3' },
 })
