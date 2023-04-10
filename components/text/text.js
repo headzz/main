ca5ef8d6c198cffabac756d95127547cea0eec71
Pedro Muniz <pedropmun.spam@gmail.com>
2023-04-09 21:21:56 -0300

    changing text component  color

--- components/text/text.js
+++ components/text/text.js
@@ -1,4 +1,5 @@
 const Text = styled('p')({
+  color: ({color}) => color ? color : '#fff',
   fontFamily:  `"Jost","Helvetica Neue",Helvetica,Arial,sans-serif`,
   fontWeight: ({weight}) => weight ? weight : 400,
   fontSize: ({size}) => size ? size : '1.6rem',
