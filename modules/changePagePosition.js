bf46e485467bae33274fb2731160df06af0a875f
Pedro Muniz <pedropmun.spam@gmail.com>
2023-04-12 08:11:15 -0300

    fixing image routes and adding component to index.html

---
+++ modules/changePagePosition.js
@@ -0,0 +1,11 @@
+const BackPagePosition = styled.div({
+  position: 'absolute',
+  bottom: "64px",
+  left: '32px'
+})
+
+const NextPagePosition = styled.div({
+  position: 'absolute',
+  bottom: "64px",
+  right: '32px'
+})
