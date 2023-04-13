aae94466b7b549bda175e54aa98bc9be351ef94c
Pedro <48931731+headzz@users.noreply.github.com>
2023-04-13 00:21:19 -0300

    changing the first screen needed

--- modules/Context/hook.js
+++ modules/Context/hook.js
@@ -1,5 +1,5 @@
 const useChangeContent = () => {
-  const [content, setContent] = React.useState('begin')
+  const [content, setContent] = React.useState('chooseSkinType')
   const [steps, setSteps] = React.useState('')
   const [product, setProduct] = React.useState('')
   return {content, setContent, steps, setSteps, product, setProduct}
